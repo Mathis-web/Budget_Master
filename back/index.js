1897324603c21b72280f1f811b53134b1d4224ad
Mathis <mathis.pati@gmail.com>
2021-12-14 07:10:50 +0100

    allow cors

--- back/index.js
+++ back/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 const router = require('./app/router');
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
